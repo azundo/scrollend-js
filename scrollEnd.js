4a2f46b129915f6269686c21e29b7c2c498f13f5
Ben Best <ben.best@gmail.com>
2013-04-19 12:22:19 +0000

    First crack at porting scrollEnd from the AVC site.

---
+++ scrollEnd.js
@@ -0,0 +1,58 @@
+ScrollEnd = (function () {
+  var ScrollEnd;
+
+  function getScrollTop() {
+      // from http://stackoverflow.com/questions/871399/cross-browser-method-for-detecting-the-scrolltop-of-the-browser-window
+        if (typeof pageYOffset != 'undefined') {
+          return pageYOffset;
+        } else {
+          var B = document.body;
+          var D = document.documentElement;
+          D = (D.clientHeight) ? D : B;
+          return D.scrollTop;
+        }
+  };
+
+  ScrollEnd = function (options) {
+    var checkInterval = (options && options.checkInterval) || 100,
+    scrolling = false,
+    lastScrollPosition = getScrollTop(),
+    i = 0;
+
+    this.subscribers = [];
+
+    setInterval(function() {
+      var scrollPosition = getScrollTop();
+      if (scrolling && scrollPosition === lastScrollPosition) {
+        // we have stopped scrolling
+        scrolling = false;
+        for (i = 0; i < subscribers.length, i++) {
+          subscribers[i](scrollPosition);
+        }
+      } else if (!scrolling && scrollPosition !== lastScrollPosition) {
+        // we have started scrolling
+        scrolling = true;
+      }
+      lastScrollPosition = scrollPosition;
+    }, checkInterval);
+  };
+
+  // subscribe to the ScrollEnd event
+  ScrollEnd.prototype.subscribe = function (func) {
+    this.subscribers.push(func);
+  };
+
+  // unsusbscribe from the ScrollEnd event
+  ScrollEnd.prototype.unsubscribe = function (func) {
+    var idx = this.subscribers.indexOf(func);
+    if (idx >= 0) {
+      this.subscribers.splice(idx, 1);
+    }
+  };
+
+  // Aliases for subscribe and unsubscribe
+  ScrollEnd.prototype.sub = ScrollEnd.prototype.subscribe;
+  ScrollEnd.prototype.unsub = ScrollEnd.prototype.unsubscribe;
+
+  return ScrollEnd;
+})();
